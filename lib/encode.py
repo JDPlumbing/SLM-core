9691f072a0523ae056c2b0ed033d0096ae0f0789
Dr.Ippy <JDPlumbingSoFlo@gmail.com>
2025-06-30 19:58:43 -0400

    push from root

--- lib/encode.py
+++
@@ -1,9 +0,0 @@
-from loader import load_dictionaries
-from utils import int_to_bytes
-
-def encode_tuple(slot_values, diction):
-    block = bytearray()
-    for slot in diction["slots"]:
-        value = slot_values.get(slot["name"], 0)
-        block += int_to_bytes(value, slot["bytes"])
-    return block
--- lib/validate.py
+++
@@ -1,4 +0,0 @@
-def validate_slot(slot_name, value, diction_map):
-    if slot_name not in diction_map:
-        return False
-    return value in diction_map[slot_name]
